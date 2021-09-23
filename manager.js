e3e0ab13b11e5cb228979e4cf9a566ed265875bb
oztar <frediroman@gmail.com>
2021-09-23 19:05:14 +0200

    First Versión

---
+++ manager.js
@@ -0,0 +1,319 @@
+/**
+ * @file Acterisk-Manager
+ * @author Alfredo Roman <alfredoromandominguez@gmail.com>
+ * @example 1  conection text
+ *  //call library
+ *  const libAMI = require('Asterisk-manager');
+ *  let ami = libAMI();
+ *
+ *  ami.login({
+ *              'hostname' : 'localhost', 
+ *              'port'     : 5038,
+ *              'username' : 'admin',
+ *              'password' : 'admin',
+ *              'ssl'      : 'disable'
+ *           }); 
+ * 
+ *  //event emitted any events of asterisk manager
+ *  ami.on('EventAny', function(a){
+ *      console.log('Event:',a);
+ *  });
+ *
+ *  //event emitted Event+<name event> of asterisk manager
+ *  // example OriginateResponse 
+ *  ami.on('EventOriginateResponse', function(a){
+ *      console.log(a);
+ *  });
+ *  
+ *  //event emitted success actions of asterisk manager
+ *  ami.on('Success', function(a){
+ *      console.log('Success',a);
+ *  });
+ *  
+ *  //error socket and error actions
+ *  ami.on('error', function(txt){
+ *      console.log('error: ',txt);
+ *  });
+ * 
+ *  let ActionID = ami.action({
+ * 	Action: 'Originate',
+ * 	Channel: 'sip/12345',
+ * 	Exten: '1234',
+ * 	Context: 'default',
+ * 	Async: 'yes'
+ *  });
+ * ...
+ *
+ * @example 2 connection TLS 
+ *  //call library
+ *  const libAMI = require('Asterisk-manager');
+ *  let ami = libAMI();
+ *   ami.login({
+ *              'hostname' : 'localhost', 
+ *              'port'     : 5038,
+ *              'username' : 'admin',
+ *              'password' : 'admin',
+ *              'ssl'      : 'enable',
+ *              'cert'               : fs.readFileSync('path/cert.pem'),
+ *              'key'                : fs.readFileSync('path/cert.key'),
+ *              'passphrase'         : 'certificateProtectKeys',
+ *              'requestCert'        : false,
+ *              'rejectUnauthorized' : false,
+ *              'secure'             : null,
+ *              'CA'                 : fs.readFileSync('path/CA.pem')
+ *           });  
+*/
+
+
+const inherits     = require('util').inherits;
+const format       = require('util').format;
+const EventEmitter = require('events').EventEmitter;
+const net          = require('net');
+const tls          = require('tls');
+
+let socket,user,pass;
+let actionID = 0;
+
+
+/**
+ * Manager Error Control
+*/
+const txtError = {
+    UNDEFINED     : 'Undefined error.',
+    EMPTY_ARGUMENT: 'Argument "%s" missing in function call.',
+    CONNECT_ERROR : 'Could not connect to server. Code: %s.',
+    SOCKED_ERROR  : 'Error in server. Code: %s.',
+    SOCKED_CLOSE  : 'Lost connection to server.',
+    AUTH_FAILED   : 'Authentication failed.',
+    ACTION_ERROR  : 'Action manager failed: %s.'
+};
+
+/**
+ * conection login
+ *
+ * @param {string} hostname - asterisk pbx server hostname or ip address
+ * @param {int} port - asterisk pbx server port number, by default is 5038
+ * @param {string} username - username (see /etc/asterisk/manager.conf)
+ * @param {string} password - password (see /etc/asterisk/manager.conf)
+ * @param {string} cert  
+ * @param {string} key
+ * @param {string} passphrase
+ * @param {string} requestCert
+ * @param {string} rejectUnauthorized
+ * @param {string} secure
+ * @param {string} CA
+ */
+function _login(_this,settings){
+    
+    /* validator settings */
+    settings.passphrase = settings.passphrase || null;
+    settings.requestCert =  settings.requestCert || false;
+    settings.rejectUnauthorized = settings.rejectUnauthorized || false;
+    settings.secure = settings.secure || null;
+    settings.CA = settings.CA || null;
+
+    let arguments_not_empty = ['hostname','port','username','password','ssl'];
+    let arguments_ssl_empty = ['cert','key'];
+
+    for( let name of arguments_not_empty){
+	if(settings[name] === undefined){
+	    _this.error(txtError['EMPTY_ARGUMENT'],name);
+            return false;
+	}
+	if( settings[name] == ''){
+	    _this.error(txtError['EMPTY_ARGUMENT'],name);
+            return false;
+	}
+    }
+
+    /* basic options */
+    _this.options = {
+	port: settings.port,
+	host: settings.hostname
+    };
+    user = settings.username;
+    pass = settings.password;
+
+    if( settings.ssl == 'on' || settings.ssl == 'enable' ){
+	for( let name of arguments_ssl_empty){
+	    if(settings[name] === undefined){
+		_this.error(txtError['EMPTY_ARGUMENT'],name);
+		return false;
+	    }
+	    if( settings[name] == ''){
+		_this.error(txtError['EMPTY_ARGUMENT'],name);
+		return false;
+	    }
+	}		
+        _this.options = {
+	    port: settings.port,
+	    host: settings.hostname,
+	    cert: settings.cert,
+	    key:  settings.key,
+	    passphrase : settings.passphrase,
+	    requestCert:  settings.requestCert,
+	    rejectUnauthorized: settings.rejectUnauthorized,
+	    secureOptions: settings.secure
+	};
+	if(  settings.CA != null){
+	    _this.options.ca = [ settings.CA ];
+	}
+    }
+    
+    process.nextTick(function(){
+	if( settings.ssl == 'on' || settings.ssl == 'enable' ){
+            _connect_ssl(_this);
+	}else{
+	    _connect(_this);
+	}
+    });
+}    
+
+function _connect_ssl(_this){
+    socket = tls.connect(_this.options, ()=>{
+	if( socket.authorized === false) {
+	    _this.error(txtError['CONNECT_ERROR'] ,socket.authorizationError);
+	}
+    });
+    _listentings(_this);
+}  
+
+
+function _connect(_this){    
+    socket = net.connect(_this.options.port, _this.options.host);
+    _listentings(_this);
+}  
+
+
+
+
+function _listentings(_this){
+    socket.setEncoding('utf8');    
+    socket.on('connect', function(){	
+	_action(_this,{
+	    Action   : 'Login',
+	    Username : user,
+	    Secret   : pass
+	});
+    });
+
+    socket.on('data', function(data){	
+	let ndata = {};
+
+	//console.log('data',data.toString());
+	let msg = data.split("\r\n");
+	for(let i in msg){
+	    let x = msg[i].split(': ');
+	    ndata[x[0]] = x[1];
+	    if(x[1] === undefined){
+		_this.send(ndata);
+		ndata={};
+	    }
+	}
+    });
+
+
+    socket.on('connecterror', function(err){
+	_this.error(txtError['CONNECT_ERROR'],err);
+    });
+    
+    socket.on('error', function(err){
+        _this.error(txtError['SOCKED_ERROR'], err.code);
+    });
+
+
+    socket.on('close', function(){
+	_this.error(txtError['SOCKED_CLOSE']);
+	_this.emit('close',1);
+    });
+}
+
+
+
+
+function _send(_this,data){
+    /* error response */
+    if( data['Response'] == 'Error'){
+	_this.error(txtError['SOCKED_ERROR'],data);
+	
+	/* data Response success */
+    }else if(data['Response'] == 'Success'){
+	_this.emit('Success', data);
+	
+	
+	/* Event response */
+    }else if(data['Event'] !== undefined){  
+        _this.emit('EventAny', data);
+        _this.emit('Event'+data['Event'], data);
+    }
+}
+
+
+function _action(_this,form){
+    if( form.Action === undefined){ 
+	_this.error(txtError['ACTION_ERROR'],'parameter action, not empty');
+	return;
+    }
+    
+    let action = '';
+    for( let id in form){
+	action += id+': '+form[id]+"\r\n";
+    }
+
+    //anadimos el action id
+    ++actionID;
+    let aid = actionID;
+    action += 'ActionID: '+aid+"\r\n";
+
+    //finalizamos
+    action += "\r\n";
+ 
+    socket.write(action);
+    return aid;
+}
+
+
+
+/**
+   * manager
+   * 
+   * pricipal constructor
+   *
+*/
+class manager extends EventEmitter{
+    options;
+
+    constructor(){
+	super();
+    }
+    
+    /* create conection to manager */
+    login(settings){
+	_login(this,settings);
+    }
+
+    /* method for ee.emit response socket to internal node */
+    send(data){
+	_send(this,data);
+    }
+
+    /* method control errors */
+    error(){
+	this.emit('error', format.apply(format,arguments));
+    }
+    
+
+    /* method for emit socket to manager */
+    action(form){
+	return _action(this,form);
+    }
+
+}
+
+
+
+
+module.exports = function(){
+    return new manager();
+
+}
---
+++ test.js
@@ -0,0 +1,260 @@
+'user strict'
+const path         = __dirname;
+const exp          = require('chai').expect;
+const lib          = require(path+ '/prueba.js');
+const net          = require('net');
+const tls          = require('tls');
+const fs           = require('fs');
+const hostname     = '127.0.0.1';
+const port         = 1337;
+const porttls      = 1338;
+
+console.log(path);
+let server,ami,res;
+
+//console.log(lib());
+let text = 'Action: Login'+"\r\n";
+text += 'Username: admin'+"\r\n";
+text += 'Secret: admin'+"\r\n";
+text += 'ActionID: 1'+"\r\n";
+text += "\r\n";
+let Originate6789 = {
+    Action : 'Originate',
+    Channel: 'sip/67890',
+    Exten: '6789',
+    Context: 'success',
+    Async: 'no'
+};
+
+
+
+describe('The tcp server', function() {
+    
+    it('create server', function(done){
+	create_server();
+	done();
+    });
+    it('server object', function(done){
+	exp(server).to.be.an('Object');
+	done();
+    });
+    it('server events', function(done){
+	exp(server._eventsCount).to.equal(1);
+	done();
+    });
+});
+
+describe('Asterisk Manager', function(){
+    ami = lib();
+
+    it('Instance Asterisk Manager', function(done){
+	ami.options = 'uno';
+	exp(ami.options).to.equal('uno');
+	done();
+    });
+    it('Asterisk Manager login', function(done){
+	let options = {
+	    'hostname' : hostname,
+	    'port'     : port,
+	    'username' : 'admin',
+	    'password' : 'admin',
+	    'ssl'      : 'off'
+      };
+
+	function test(data){
+	    exp(data.Response).to.equal('Success');
+	    ami.off('Success',test);
+	    done();
+	}
+	ami.on('Success',test);
+	ami.login(options);
+    });
+    
+    it('Asterisk Manager Action', function(done){
+	
+	let ActionID = ami.action(Originate6789);
+	exp(ActionID).to.equal(2);
+	done();
+    });
+    it('Asterisk Manager Event', function(done){
+
+	let ActionID = ami.action(Originate6789);
+	function event(data){
+	    exp(data.Event).to.equal('OriginateResponse');
+	    ami.off('EventAny',event );
+	    done();
+	}
+	ami.on('EventAny',event );	
+    });  
+
+    it('Asterisk Manager EventOriginate', function(done){
+	let ActionID = ami.action(Originate6789);
+	function eventO(data){
+	    exp(data.Event).to.equal('OriginateResponse');
+	    ami.off('EventOriginateResponse',eventO);
+	    done();
+	}	
+	ami.on('EventOriginateResponse',eventO);
+    });
+});
+describe('Close tcp server', function() {
+ it('Close ', function(done){
+     function fclose(data){
+	 exp(data).to.equal('Lost connection to server.');
+	 ami.off('error',fclose);
+	 done();
+     }
+     ami.on('error',fclose);
+ });
+});
+describe('The TLS server', function() {
+    
+    it('create server', function(done){
+	create_tls_server();
+	done();
+    });
+    it('server object', function(done){
+	exp(server).to.be.an('Object');
+	done();
+    });
+    it('server events', function(done){
+	exp(server._eventsCount).to.equal(2);
+	done();
+    });
+});
+
+
+
+describe('Asterisk Manager TLS', function(){
+    ami = lib();
+
+    it('Instance Asterisk Manager', function(done){
+	ami.options = 'uno';
+	exp(ami.options).to.equal('uno');
+	done();
+    });
+    it('Asterisk Manager TLS Error login', function(done){
+	let options = {
+	    'hostname' : hostname,
+	    'port'     : porttls,
+	    'username' : 'admin',
+	    'password' : 'admin',
+	    'ssl'      : 'enable',
+	    'cert'     : fs.readFileSync('ssl/client.pem'),
+	    'key'      : fs.readFileSync('ssl/client.key'),
+	    'requestCert'        : false,
+	    'rejectUnauthorized' : false,
+	    'ca'       : [fs.readFileSync('ssl/ca.pem')]
+      };
+
+	function test(data){
+	    //SELF_SIGNED_CERT_IN_CHAIN  its a certificate error, non a library error.
+	    //this library correct works. 
+	    //This not error server TLS
+	    exp(data).to.equal('Could not connect to server. Code: SELF_SIGNED_CERT_IN_CHAIN.');
+	    ami.off('error',test);
+	    done();
+	}
+	ami.on('error',test);
+	ami.login(options);
+    });
+    
+    it('Asterisk Manager Action', function(done){
+	
+	let ActionID = ami.action(Originate6789);
+	exp(ActionID).to.equal(6);
+	done();
+    });
+    it('Asterisk Manager Event', function(done){
+
+	let ActionID = ami.action(Originate6789);
+	function event(data){
+	    exp(data.Event).to.equal('OriginateResponse');
+	    ami.off('EventAny',event );
+	    done();
+	}
+	ami.on('EventAny',event );	
+    });  
+
+    it('Asterisk Manager EventOriginate', function(done){
+	let ActionID = ami.action(Originate6789);
+
+	function eventO(data){
+	    exp(data.Event).to.equal('OriginateResponse');
+	    ami.off('EventOriginateResponse',eventO);
+	    done();
+	}
+	ami.on('EventOriginateResponse',eventO);
+    });
+});
+
+describe('Close tls server', function() {
+ it('Close ', function(done){
+     function fclose(data){
+	 exp(data).to.equal('Lost connection to server.');
+	 ami.off('error',fclose);
+	 done();
+     }
+     ami.on('error',fclose);
+ });
+    after(function () {
+	process.exit(1);
+    });
+});
+
+
+async function create_server(){
+    server = await net.createServer(function(socket){
+	socket = listenings(socket);
+    });    
+    server.listen(port,hostname);
+
+}
+async function create_tls_server(){
+    // Private key and public certificate for access
+    var options = {
+	key: fs.readFileSync('ssl/server.key'),
+	cert: fs.readFileSync('ssl/server.pem'),
+	requestCert        : false,
+	rejectUnauthorized : false,
+	ca: fs.readFileSync('ssl/ca.pem')
+    };
+    server = tls.createServer(options, function(socket) {
+	socket = listenings(socket);
+    });    	
+    server.listen(porttls,hostname);
+    setTimeout( ()=>{
+	server.close();
+    },4000);
+}
+
+
+function listenings(socket){
+    socket.setEncoding('utf8');
+    try{ 
+	socket.on('data', function(data){
+	    let response = {
+		'login' : 'Response: Success'+"\r\n"+'ActionID: 1'+"\r\n"+'Message: Authentication accepted'+"\r\n",
+		'originate' : 'Response: Success'+"\r\n"+'ActionID: 2'+"\r\n"+'Message: Originate successfully queued'+"\r\n",
+		'Oresponse' : 'Event: OriginateResponse'+"\r\n"+'Privilege: call,all'+"\r\n"+'ActionID: 2'+"\r\n"+'Response: Failure '+"\r\n"+'Channel: sip/12345'+"\r\n"+'Context: default'+"\r\n"+'Exten: 1234'+"\r\n"+'Reason: 0'+"\r\n"
+	    }
+	    let msg = data.toString().split("\r\n");
+	    if( msg[0] == 'Action: Login'){
+		socket.write(response.login);
+	    }else if( msg[0] == 'Action: Originate'){
+		socket.write(response.originate);
+		setTimeout( ()=>{
+		    socket.write(response.Oresponse);
+		},1500);
+	    }
+	});
+    }catch(err){
+	console.log(err);
+    }
+    setTimeout( ()=>{
+	socket.destroy();
+    },4000);
+    return socket;
+}
+
+
